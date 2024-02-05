83058c84475f85bc1fa131411b8f5601de553c5d
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-02-05 16:05:26 -0500

    updating user model and seeds

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,11 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
+    type=db.Column(db.String(40), nullable=False)
+    condo_id=db.Column(db.Integer,nullable=False)
+    secret_key=db.Column(db.String,nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
@@ -28,6 +32,9 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
+            'first_name': self.first_name,
+            'last_name':self.last_name,
+            'type':self.type,
+            'condo_id':self.condo_id,
             'email': self.email
         }
