7d51c8fc6d1e436367066515fe748a2e412224ee
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-05-03 15:37:26 -0400

    added talisman enabled

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY="mysecret"
     SESSION_COOKIE_SAMESITE=None
+    TALISMAN_ENABLED="False"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
