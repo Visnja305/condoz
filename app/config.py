d0cdf74fcb5de7f4be8ea5899be78273377acf92
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-05-03 15:33:35 -0400

    added session_cookie_samesite

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY="mysecret"
+    SESSION_COOKIE_SAMESITE=None
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
