adb346cb130045c663539289854c2ce399b746de
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-06-04 16:26:41 -0400

    fixed the navigation after logout

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector,useDispatch } from 'react-redux';
 import {logout} from "../../store/session";
 import { Navigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import "./NavBar.css";
 /* eslint-disable react/prop-types */
@@ -9,13 +10,14 @@ import "./NavBar.css";
 function NavBar({ isLoaded }){
 	// const history=useHistory();
     const dispatch=useDispatch();
+	let navigate = useNavigate();
 
 	const sessionUser = useSelector(state => state.session.user);
 const handleLogOut=async (e)=>{
     e.preventDefault();
-    await dispatch(logout());
+    await dispatch(logout()).then(()=>navigate("/"));
 
-	<Navigate to="/" />
+	// <Navigate to="/" />
 }
 	return (<>
         {sessionUser &&
