28d6daa0d99aed09bb875ecfd5d163513d293246
Visnja Vracaric <visnja.vracaric@gmail.com>
2024-06-22 16:41:10 -0400

    map testing done

--- frontend/src/components/UserProfilePage/UserProfilePage.jsx
+++ frontend/src/components/UserProfilePage/UserProfilePage.jsx
@@ -8,6 +8,7 @@ import CreateUserProfileModal from "../CreateUserProfileModal";
 import { getCondosThunk } from "../../store/condos";
 import {getUsersThunk} from "../../store/users";
 import ShowEvents from "../ShowEvents";
+import { Loader } from "@googlemaps/js-api-loader"
 
 const UserProfilePage =()=>{
 
@@ -95,6 +96,116 @@ setSendInterest(checkedInterest);
 
 
 // }
+
+
+
+
+const loader = new Loader({
+    apiKey: "AIzaSyDT-2eTsngiC7PYb6IWkBmtnfi8giJ5-wg",
+    version: "weekly",
+    libraries:"places",
+    callback:"initAutocomplete"
+
+  });
+
+
+
+
+
+const initAutocomplete= async ()=> {
+    const { Map } = await google.maps.importLibrary("maps");
+    const map = new Map(document.getElementById("map"), {
+      center: { lat: -33.8688, lng: 151.2195 },
+      zoom: 13,
+      mapTypeId: "roadmap",
+    });
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    let markers = [];
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+
+      places.forEach((place) => {
+        console.log(place.geometry.location?.lng())
+      console.log(place.geometry?.location?.lat())
+        if (!place.geometry || !place.geometry.location) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          }),
+        );
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
+  loader.load().then(initAutocomplete());
+//   window.initAutocomplete = initAutocomplete;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return(<div className="user-profile-page-base">{sessionUser && <div className="if-session-user-profile-page-base">{sessionUser.has_profile==="no" &&<div className="if-user-has-profile-page">
         <p>Join the community</p>
@@ -193,7 +304,46 @@ setSendInterest(checkedInterest);
 
 
 
-    </div>}</div>  }</div>)
+    </div>}</div>  }
+<div className="map-container">
+    <div className="input-maps">
+    <input
+      id="pac-input"
+      class="controls"
+      type="text"
+      placeholder="Search Box"
+    />
+    </div>
+    <div id="map"></div>
+
+
+    </div>
+
+
+
+
+
+
+
+{/* 111111111111
+    <div id="map"></div>
+   { (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyDT-2eTsngiC7PYb6IWkBmtnfi8giJ5-wg", v: "weekly"})} */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>)
 }
 
 export default UserProfilePage;
